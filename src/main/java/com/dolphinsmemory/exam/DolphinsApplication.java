e7702e1913e906606a170f49b3e06dce16b8e798
Georgy <louis.georgy@gmail.com>
2018-09-04 14:18:01 -0400

    update

--- src/main/java/com/dolphinsmemory/exam/DolphinsApplication.java
+++ src/main/java/com/dolphinsmemory/exam/DolphinsApplication.java
@@ -1,7 +1,12 @@
 package com.dolphinsmemory.exam;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.dolphinsmemory.exam.model.Student;
+import com.dolphinsmemory.exam.repository.StudentRepository;
 
 @SpringBootApplication
 public class DolphinsApplication {
@@ -9,4 +14,12 @@ public class DolphinsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DolphinsApplication.class, args);
 	}
+	
+//	@Bean
+//	public CommandLineRunner demo(StudentRepository studentRepository) {
+//		
+//		return(args) -> {
+//			studentRepository.save(new Student("","first","last","email","11/12/12","username","domain","grade","f",""));
+//		};
+//	}
 }
